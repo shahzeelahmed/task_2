85b6a4187f926c7707e9cd746bc1702f7e2771a7
shahzeelahmed <ahmedshahzeel@gmail.com>
2024-03-09 20:19:30 +0530

    refactor

--- lib/src/constants/util.dart
+++ lib/src/constants/util.dart
@@ -9,8 +9,5 @@ class Util {
   static double splashAspectRatio = 2864 / 3000;
   static double lgZoomScale = 130000000.0;
   
-  static CameraPosition initialMapPosition = const CameraPosition(
-    target: LatLng(51.4769, 0.0),
-    zoom: 2,
-  );
+
 }
